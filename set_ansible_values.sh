eedaac95d7e5045e32b7637f81260d66dce5cc09
Warren Pearson <warren@insightcapital.io>
2020-12-26 13:30:11 +0100

    fix config

--- set_ansible_values.sh
+++ set_ansible_values.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-terraform output | grep rds | sed -e 's/ =/:/' | sed -e 's/"//g' |sort > ../simple-node-mysql-app-ansible/roles/node-app/defaults/main.yml
+echo "---" > ../simple-node-mysql-app-ansible/roles/node-app/defaults/main.yml
+echo "http_port: 80" >> ../simple-node-mysql-app-ansible/roles/node-app/defaults/main.yml
 
-rm ../simple-node-mysql-app-ansible/hosts
+terraform output | grep rds | sed -e 's/ =/:/' | sed -e 's/"//g' |sed -e 's/:3306//' |sort >> ../simple-node-mysql-app-ansible/roles/node-app/defaults/main.yml
+
+rm -f ../simple-node-mysql-app-ansible/hosts
 
 terraform output |grep ec2 |cut -d\" -f2 > ../simple-node-mysql-app-ansible/hosts
